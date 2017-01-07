3ad9b15d6c33aa3ee510c75d96fa4d1d7ee67ad6
sah3122 <sah3122@naver.com>
2017-01-07 15:11:53 +0900

    quartz 수정

--- homepage/src/main/java/ks/mic/quartz/controller/AchievementQuartzController.java
+++ homepage/src/main/java/ks/mic/quartz/controller/AchievementQuartzController.java
@@ -7,17 +7,21 @@ import javax.annotation.Resource;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import ks.mic.quartz.service.AchievementQuartzService;
+import ks.mic.quartz.service.CheckQuartzService;
 
 public class AchievementQuartzController extends QuartzJobBean{
-	@Resource(name="achievementQuartzService")
+	private ApplicationContext ctx;
 	private AchievementQuartzService achievementQuartzService;
 	
 	@Override
 	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
+		ctx = (ApplicationContext) arg0.getJobDetail().getJobDataMap().get("applicationContext");
+		achievementQuartzService = (AchievementQuartzService) ctx.getBean("AchievementQuartzService");
 		Map<String, Object> data = new HashMap<String, Object>();
 		try {
 			achievementQuartzService.updateAchievementData(data);
--- homepage/src/main/java/ks/mic/quartz/controller/CheckQuartzController.java
+++ homepage/src/main/java/ks/mic/quartz/controller/CheckQuartzController.java
@@ -3,21 +3,22 @@ package ks.mic.quartz.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import ks.mic.quartz.service.CheckQuartzService;
 
-public class CheckQuartzController extends QuartzJobBean{
-	@Resource(name="checkQuartzService")
+public class CheckQuartzController extends QuartzJobBean {
+	private ApplicationContext ctx;
 	private CheckQuartzService checkQuartzService;
-	
+
 	@Override
 	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
 		// TODO Auto-generated method stub
+		ctx = (ApplicationContext) arg0.getJobDetail().getJobDataMap().get("applicationContext");
+		checkQuartzService = (CheckQuartzService) ctx.getBean("CheckQuartzService");
 		Map<String, Object> data = new HashMap<String, Object>();
 		try {
 			checkQuartzService.updateCheckData(data);
@@ -26,5 +27,4 @@ public class CheckQuartzController extends QuartzJobBean{
 			e.printStackTrace();
 		}
 	}
-
 }
